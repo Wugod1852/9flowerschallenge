7ad75a9cf1c4ce9f104c148f191cab384a9d1a6d
Wugod1852 <40473911+Wugod1852@users.noreply.github.com>
2018-07-09 09:29:50 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var drawing = SVG('drawing-area');
+drawing
+  .rect(300,200)
+drawing
+  .rect(300,200)
+  .fill('red')
+drawing
+  .ellipse(200, 200)
+  .fill('pink')
+  .center(300,300)
+drawing
+  .rect(100,300)
+  .fill('blue')
+  .center(300,600)
+  
